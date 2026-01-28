238c12207f064221c1a7495e6c390b2eaa510664
EvanGudmestad <eagudmestad@ranken.edu>
2026-01-28 14:15:52 -0600

    added default MVC routing

--- Program.cs
+++ Program.cs
@@ -2,5 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 app.MapStaticAssets();
-app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
