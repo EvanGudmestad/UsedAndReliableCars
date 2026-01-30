c66c1b0e903e080ded15db4456fb5f235937caef
BrodyBoyd <Brody_Boyd@insideranken.org>
2026-01-30 13:33:21 -0600

    landing page

---
+++ Models/UsedCar.cs
@@ -0,0 +1,14 @@
+﻿namespace UsedAndReliableCars.Models
+{
+    public class UsedCar
+    {
+        public string Type { get; set; }
+        public string PriceCategory { get; set; }
+        public string Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public List<UsedCar> UsedCars { get; set; }
+
+        
+    }
+}
