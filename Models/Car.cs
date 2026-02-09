a7e1a962832128568a58ff9e281381089a7c98bb
Michael-Rhoades <Michael_Rhoades@insideranken.org>
2026-02-09 15:59:13 -0600

    Added basic layout

---
+++ Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace UsedAndReliableCars.Models
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double price { get; set; }
+    }
+}
